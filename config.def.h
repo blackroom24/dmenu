a919abd91189089ab5644f842702d6ed36ab2695
Sanket Debnath <sanketdebnath24@gmail.com>
2020-05-02 23:20:13 +0530

    added nord colors and mononoki nerd font as default

--- config.def.h
+++ config.def.h
@@ -4,15 +4,15 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"Mononoki Nerd Font:size=13"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeSelHighlight] = { "#ffc978", "#005577" },
-	[SchemeNormHighlight] = { "#ffc978", "#222222" },
+	[SchemeNorm] = { "#8fbcbb", "#3b4252" },
+	[SchemeSel] = { "#3b4252", "#8fbcbb" },
+	[SchemeSelHighlight] = { "#bf616a", "#8fbcbb" },
+	[SchemeNormHighlight] = { "#bf616a", "#3b4252" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
